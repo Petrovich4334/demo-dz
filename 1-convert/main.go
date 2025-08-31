830081326cd170d69215d9950e6b849727737254
Peter <petrovich43@gmail.com>
2025-08-31 16:54:50 +0300

    Мой второй коммит DZ 1

---
+++ 1-convert/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("__Калькулятор валют__")
+
+	const usdToEur = 0.85
+	const usdToRub = 80.33
+
+	var amount float64
+	fmt.Print("Введите сумму в USD: ")
+	fmt.Scan(&amount)
+
+	eurAmount := amount * usdToEur
+	rubAmount := amount * usdToRub
+
+	fmt.Printf(`Результаты конвертации:
+	%.2f USD = %.2f EUR
+	%.2f USD = %.2f RUB
+	`, amount, eurAmount, amount, rubAmount)
+}
