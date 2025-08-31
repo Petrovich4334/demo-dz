4098d33d07e284c4db399867e9464dd22ba34eef
Peter <petrovich43@gmail.com>
2025-08-31 16:59:16 +0300

    Мой третий коммит DZ 3

--- 1-convert/main.go
+++ 1-convert/main.go
@@ -1,31 +1,109 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 func main() {
 	fmt.Println("__Калькулятор валют__")
 
+	// Получение всей информации от пользователя
+	fromCurrency, err := getCurrencyInput("Введите исходную валюту  (USD, EUR, RUB): ")
+	if err != nil {
+		fmt.Println("Ошибка ввода валюты:", err)
+		return
+	}
+
+	amount, err := getAmountInput("Введите сумму: ")
+	if err != nil {
+		fmt.Println("Ошибка ввода суммы", err)
+		return
+	}
+
+	toCurrency, err := getCurrencyInput("Введите целевую валюту (USD, EUR, RUB): ")
+	if err != nil {
+		fmt.Println("Ошибка ввода валюты:", err)
+		return
+	}
+
+	// Конвертация валюты
+	result := convertCurrency(amount, fromCurrency, toCurrency)
+
+	// Вывод результатов конвертации
+	fmt.Printf("%.2f %s = %.2f %s\n", amount, fromCurrency, result, toCurrency)
+}
+
+// Функция для получения ввода валюты от пользователя
+func getCurrencyInput(prompt string) (string, error) {
+	var currency string
+	for {
+		fmt.Print(prompt)
+		_, err := fmt.Scan(&currency)
+		if err != nil {
+			return "", errors.New("ошибка чтения ввода")
+		}
+
+		// Проверка всех возможных вариантов написания USD
+		switch currency {
+		case "USD", "usd":
+			return "USD", nil
+		case "EUR", "eur":
+			return "EUR", nil
+		case "RUB", "rub":
+			return "RUB", nil
+		default:
+			fmt.Println("Недопустимая валюта, используйте USD, EUR или RUB")
+		}
+	}
+}
+
+// Функция для получения суммы от пользователя
+func getAmountInput(prompt string) (float64, error) {
+	var amount float64
+
+	for {
+		fmt.Print(prompt)
+		_, err := fmt.Scan(&amount)
+
+		if err != nil {
+			fmt.Println("Ошибка ввода, попробуйте снова.")
+			continue
+		}
+
+		if amount < 0 {
+			fmt.Println("Сумма не может быть отрицательной, поробуйте снова")
+			continue
+		}
+		return amount, nil
+	}
+}
+
+// Функция для конвертации валют
+func convertCurrency(amount float64, fromCurrency string, toCurrency string) float64 {
 	const usdToEur = 0.85
 	const usdToRub = 80.33
 
-	amount := getAmountInput("Введите сумму в USD: ")
+	var inUSD float64
 
-	eurAmount := amount * usdToEur
-	rubAmount := amount * usdToRub
+	// Конвертируем исходную валюту в USD используя tagged switch
+	switch fromCurrency {
+	case "USD":
+		inUSD = amount
+	case "EUR":
+		inUSD = amount / usdToEur
+	case "RUB":
+		inUSD = amount / usdToRub
+	}
 
-	fmt.Printf(`Результаты конвертации:
-	%.2f USD = %.2f EUR
-	%.2f USD = %.2f RUB
-	`, amount, eurAmount, amount, rubAmount)
-}
-
-func getAmountInput(prompt string) float64 {
-	var amount float64
-	fmt.Print(prompt)
-	fmt.Scan(&amount)
-	return amount
-}
-
-func convertCurrency(amount float64, fromCurrency string, toCurrency string) float64 {
+	// Конвертируем из USD в целевую валюту используя tagged switch
+	switch toCurrency {
+	case "USD":
+		return inUSD
+	case "EUR":
+		return inUSD * usdToEur
+	case "RUB":
+		return inUSD * usdToRub
+	}
 	return 0
 }
